1a20aac847030250f558508d9c8ea8178b3a72a0
sovii <150125982+sophieboop@users.noreply.github.com>
2024-04-26 10:01:33 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,1 +1,20 @@
-erm
+import os
+print("How many total physical cores are on your system? (Task Manager / 2)")
+physcores = input("Phsical Cores:")
+physcoresint = int(physcores)
+if physcoresint < 3:
+  print("Running on Dual / Single core is not supported! ")
+print("Does Core 0,1 or 2 Have L1 Cache access? (check lstopo)")
+cachecore = input("First Core with L1:")
+cachecoreint = int(cachecore)
+print("Please select one of the following performance modes:")
+print("1 - Low Performance")
+print("2 - Balanced Performance")
+print("3 - High Performance")
+selection = int(input("Mode:"))
+if selection = 1:
+
+
+for x in range(physcoresint):
+  print(x)
+
